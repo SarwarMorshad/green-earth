ef4b112f6a4c2306d200021eda8504555da2409f
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-09-10 11:20:27 +0200

    Categories Displayed from API

--- script/script.js
+++ script/script.js
@@ -1,1 +1,21 @@
-console.log("console log from script.js");
+//  ! Load Categories from API
+const loadCategories = () => {
+  const url = `https://openapi.programming-hero.com/api/categories`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories));
+};
+
+// ! Display Categories
+const displayCategories = (categories) => {
+  console.log(categories);
+  const categoriesContainer = document.getElementById("categories-container");
+  categoriesContainer.innerHTML = "";
+  categories.forEach((category) => {
+    const categoryDiv = document.createElement("div");
+    categoryDiv.innerHTML = `<li class="text-lg mb-2 cursor-pointer"><a >${category.category_name}</a></li>`;
+    categoriesContainer.appendChild(categoryDiv);
+  });
+};
+
+loadCategories();
